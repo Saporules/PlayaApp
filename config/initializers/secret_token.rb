8b4b3291651f575e04a934c38e09e0b5743e4cd5
asaf eduardo lopez govea <lguaperrimo@gmail.com>
2014-01-31 13:55:18 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlayaApp::Application.config.secret_key_base = 'ac2dfccf9b1442cf265f975bb85669e0f4db9aa5d708a219e1bf26b0b07747f11c5b1111fdab7571552222ba508bfcc2892cbf812594547dfd5bbaeb1fb6799c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PlayaApp::Application.config.session_store :cookie_store, key: '_PlayaApp_session'
